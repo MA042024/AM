ce9dfcb34740b811c19570e70dc1695ae97d1123
MA042024 <mohammad.abbas@empa.ch>
2024-05-16 12:50:46 +0200

    Update Pages views.py
    Update mdcs/views.py

---
+++ mdcs/views.py
@@ -0,0 +1,28 @@
+import json
+
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+from django.middleware.csrf import CsrfViewMiddleware
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def gensel_view(request):
+    return render(request, 'gensel.html')
+
+def gensel_edit(request):
+    
+    edit_record_data = request.session.get('edit_record_data')
+
+    if not edit_record_data:
+        return HttpResponseBadRequest("No data found in session.")
+    
+    context = {
+        'data_id_edit': edit_record_data.get('data_id'),
+        'data_content_edit': edit_record_data.get('data_content'),
+        'data_title_edit': edit_record_data.get('data_title'),
+        'edit': edit_record_data.get('edit'),
+    }
+    
+    return render(request, 'gensel.html', context)
