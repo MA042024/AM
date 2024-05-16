f5d4c9a26bc15990794159a0e592506cf2f5eeb4
MA042024 <mohammad.abbas@empa.ch>
2024-05-16 12:05:28 +0200

    Update urls.py

--- mdcs_home/urls.py
+++ mdcs_home/urls.py
@@ -8,5 +8,5 @@ from mdcs_home import views
 urlpatterns = [
     re_path(r"^tiles", views.tiles, name="mdcs_home_tiles"),
     re_path(r"^templates", views.template_list, name="mdcs_home_templates"),
-    re_path(r"^gensel/", views.gensel_view, name="gensel"),
+    re_path(r"^gensel", views.gensel_view, name="mdcs_home_gensel"),
 ]
