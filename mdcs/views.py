bd0900f2291e49dd5aee1c75743e0b7b903b05f7
MA042024 <mohammad.abbas@empa.ch>
2024-06-20 20:11:15 +0200

    Update views.py

--- mdcs/views.py
+++ mdcs/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
-
+from django.views.decorators.csrf import csrf_protect
 
 def gensel_view(request):
     return render(request, 'gensel.html')
 
+@csrf_protect
 @require_POST
 def gensel_recieve(request):
     data_id = request.POST.get('data_id')
