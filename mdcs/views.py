2baaa071147e0dbb638dc94b3f5c063884ea5ec8
MA042024 <mohammad.abbas@empa.ch>
2024-06-20 16:24:37 +0200

    Update views.py

--- mdcs/views.py
+++ mdcs/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+
 
 def gensel_view(request):
     return render(request, 'gensel.html')
 
+@require_POST
+def gensel_recieve(request):
+    data_id = request.POST.get('data_id')
+    data_content = request.POST.get('data_content')
+
+    # Process the received data as needed
+    # For example, you might save it to the database or perform some action
+
+    # Return a response
+    return JsonResponse({'message': 'Data received successfully', 'data_id': data_id, 'data_content': data_content})
