f658f6cf0d8615683f9d2dd958f2e72d0fad534e
MA042024 <mohammad.abbas@empa.ch>
2024-06-21 10:31:27 +0200

    Update views.py

--- mdcs/views.py
+++ mdcs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
-from django.views.decorators.csrf import @csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 
 def gensel_view(request):
     return render(request, 'gensel.html')
