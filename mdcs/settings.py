547ed7801116bcb4681087c5a4681807be385d59
MA042024 <mohammad.abbas@empa.ch>
2024-06-24 11:39:44 +0200

    Update settings.py

--- mdcs/settings.py
+++ mdcs/settings.py
@@ -136,6 +136,7 @@ MIDDLEWARE = (
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "core_main_app.middleware.timezone.TimezoneMiddleware",
+    "core_main_app.middleware.Debug",
 )
 
 TEMPLATES = [
