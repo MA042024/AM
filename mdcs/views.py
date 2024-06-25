031b4f62f0a61694f2796c17d7508001af0c8585
MA042024 <mohammad.abbas@empa.ch>
2024-06-25 10:44:51 +0200

    Update views.py

--- mdcs/views.py
+++ mdcs/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
@@ -8,17 +8,3 @@ from django.middleware.csrf import CsrfViewMiddleware
 
 def gensel_view(request):
     return render(request, 'gensel.html')
-
-@csrf_exempt
-def gensel_show(request):
-    data_id = request.session.get('data_id')
-    data_content = request.session.get('data_content')
-
-    print(f"I got: {data_id}")
-    
-    context = {
-        'data_id': data_id,
-        'data_content': data_content,
-    }
-    
-    return render(request, 'gensel.html', context)
