9f9c020748928d73601f5bf234f796d6e0e78241
MA042024 <mohammad.abbas@empa.ch>
2024-07-01 11:11:56 +0200

    Update views.py

--- mdcs/views.py
+++ mdcs/views.py
@@ -21,11 +21,16 @@ def gensel_edit(request):
     print(f"Titttt:{edit_record_data.get('data_title')}")
     
 
+    #context = {
+    #    'data_id_edit': edit_record_data.get('data_id'),
+    #    'data_content_edit': edit_record_data.get('data_content'),
+    #    'data_title_edit': edit_record_data.get('data_title'),
+    #    'edit': edit_record_data.get('edit'),
+    #}
+
     context = {
-        'data_id_edit': edit_record_data.get('data_id'),
-        'data_content_edit': edit_record_data.get('data_content'),
-        'data_title_edit': edit_record_data.get('data_title'),
-        'edit': edit_record_data.get('edit'),
+        'edit_record_data': edit_record_data
     }
+    
 
     return render(request, 'gensel.html', context)
